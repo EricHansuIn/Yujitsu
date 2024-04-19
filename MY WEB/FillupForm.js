a1cd766a28a7991f737d5b45a2aa74a70fb40f47
EricHansuIn <167566039+EricHansuIn@users.noreply.github.com>
2024-04-19 10:36:31 -0700

    Add files via upload

---
+++ MY WEB/FillupForm.js
@@ -0,0 +1,5 @@
+document.getElementById("fill-up-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Display submit success message
+    alert("SUBMIT SUCCESS!");
+});
